e591acd73ea3842a54dbcc1862268d8db3b0171b
chloeWieser <chloerosewieser@gmail.com>
2022-04-26 01:26:22 -0400

    removed app.css

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+
+
+function App() {
+    return (
+        <>
+            <div class="container-fluid col-6 offset-3 py-5">
+
+                <div class="row">
+                    <div class="col">
+                        <h1 class="text-center">Contact List</h1>
+                    </div>
+
+                </div>
+
+
+                <div>
+                    <div class="row">
+                        <div class="paper mx-auto my-5 fontStyle">
+                            <form class="col-12">
+                                <div class="input-group" height="auto" width="100%">
+
+                                    <div class="col-12">
+                                        <input name="contactInput" class="form-control form-control-lg"
+                                            placeholder="search contacts" id="contactInput"
+                                            type="text"></input>
+                                    </div>
+
+                                    <div class="row mt-2">
+                                        <div class="col-12">
+
+                                            <button class="btn btn-info h-50" type="button"
+                                                id="addNew" name="addNew">Add New Contact</button>
+
+                                            <button class="btn btn-warning h-50 m-4" type="button"
+                                                id="showFaves" name="showFaves">Show Favorites</button>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </form>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <div class="col">
+                        <div class="flatPaper mx-auto mb-5 fontStyle">
+                            Previously entered contacts
+                            <ul>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default App;
